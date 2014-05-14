c6d990569e8a7ce2bd4fc0c3633b8bd4fca934c8
Matt Felsen <mattfelsen@gmail.com>
2014-05-14 22:33:32 +0000

    Fixed path to web directory after node folder re-structuring

--- node/server/http.js
+++ node/server/http.js
@@ -1,6 +1,6 @@
 var express = require("express");
 var app = express();
 
-app.use(express.directory("../web"));
-app.use(express.static("../web"));
+app.use(express.directory("../../web"));
+app.use(express.static("../../web"));
 app.listen(9000);
